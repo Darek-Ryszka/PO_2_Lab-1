083fd667f3ec8803b925ddaab128eb013e53c266
Dariusz Ryszka <dariusz.ryszka@onet.pl>
2022-10-22 17:41:44 +0200

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+import javax.swing.JOptionPane;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+       JOptionPane.showMessageDialog(null, "Coś", "tytuł", JOptionPane.PLAIN_MESSAGE);
+    }
+}
